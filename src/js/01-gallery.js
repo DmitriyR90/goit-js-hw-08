1a7ed2a279f15b75611dde5efc9ee1e8a820225a
DmitriyR90 <ryabokon.dn@gmail.com>
2022-08-05 15:58:51 +0300

    stage1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+
+const createGallery = createGalleryMarkup(galleryItems);
+
+galleryConteiner.innerHTML = createGallery;
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>`
+    )
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
