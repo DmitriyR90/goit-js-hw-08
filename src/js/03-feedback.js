b08798bc101e2820d4e8ad5327dc4340a9e9c542
DmitriyR90 <ryabokon.dn@gmail.com>
2022-08-07 01:04:26 +0300

    part2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  emailArea: document.querySelector('.feedback-form input'),
+  messageArea: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackData = {};
+
+populateFeedbackFormData();
+
+refs.feedbackForm.addEventListener('submit', onFormSubmit);
+refs.feedbackForm.addEventListener('input', throttle(onFeedbackInput, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (refs.emailArea.value === '' || refs.messageArea.value === '') {
+    alert('Заполните все строки');
+  } else {
+    console.log(feedbackData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function onFeedbackInput(e) {
+  feedbackData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackData));
+}
+
+function populateFeedbackFormData() {
+  const savedFeedback = localStorage.getItem(STORAGE_KEY);
+
+  if (savedFeedback) {
+    savedFeedbackData = JSON.parse(savedFeedback);
+    const { message = '', email = '' } = savedFeedbackData;
+    refs.emailArea.value = email;
+    refs.messageArea.value = message;
+  }
+}
